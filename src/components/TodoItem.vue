4c99dac888ce785ba2c3d32da0ae532ee2433bea
“wzy <1757629766@qq.com>
2018-08-30 19:10:28 +0800

    实现简单的todoList

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li class="item" @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+ 	props:['content','index'],
+ 	methods:{
+ 		handleDelete(){
+ 				this.$emit('delete',this.index)
+ 		}
+ 	}
+}
+</script>
+<!--通过 scoped修饰符 增加css的作用域为当前组件-->
+<style scoped>
+	.item{
+		color:#0f0
+	}
+</style>
